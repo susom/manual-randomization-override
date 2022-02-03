191003b48e48c2817ad2e36172cbf4da175fe7ed
Andy Martin <andy123@stanford.edu>
2022-02-03 14:00:37 -0800

    cleaning input vars

--- ajax/handler.php
+++ ajax/handler.php
@@ -4,12 +4,20 @@ namespace Stanford\RandomizerOveride;
 
 use REDCap;
 
+function clean($var) {
+    if(is_array($var)) {
+        array_map("clean",$var);
+    } else {
+        $var = htmlentities(strip_tags($var),ENT_QUOTES);
+    }
+    return $var;
+}
 
-$action                 = !empty($_POST["action"]) ? $_POST["action"] : null;
-$source_fields          = isset($_POST["source_fields"])  ? $_POST["source_fields"] : NULL ;
-$strata_fields          = isset($_POST["strata_fields"])  ? $_POST["strata_fields"] : NULL ;
-$strata_source_lookup   = array_flip($strata_fields);
-$record_id              = isset($_POST["record_id"])  ? $_POST["record_id"] : NULL ;
+$action               = !empty($_POST["action"])       ? clean($_POST["action"])        : NULL;
+$source_fields        = isset($_POST["source_fields"]) ? clean($_POST["source_fields"]) : NULL;
+$strata_fields        = isset($_POST["strata_fields"]) ? clean($_POST["strata_fields"]) : NULL;
+$strata_source_lookup = array_flip($strata_fields);
+$record_id            = isset($_POST["record_id"])     ? clean($_POST["record_id"])     : NULL ;
 
 
 switch($action){
@@ -17,7 +25,7 @@ switch($action){
         // Take the current instruments strata values (which may be unsaved)
 
         // check record for remaining strata fields outside of this instrument
-        $check_fields   = isset($_POST["check_fields"])  ? $_POST["check_fields"] : NULL ;
+        $check_fields   = isset($_POST["check_fields"]) ? clean($_POST["check_fields"]) : NULL;
 
         $q              = REDCap::getData('json', array($record_id) , $check_fields);
         $results        = json_decode($q,true);
